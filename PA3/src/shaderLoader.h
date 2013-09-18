38e5f6890eb2e272ffb627598f53679661a1c878
Denver Liu <denver@super-orange.(none)>
2013-09-18 00:44:28 -0700

    PA3 Complete

---
+++ PA3/src/shaderLoader.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+ 
+char *readFile(char *fileName) 
+{
+ 
+    FILE *filePointer;
+    char *content = NULL;
+ 
+    int count=0;
+ 
+    if (fileName != NULL) 
+    {
+        filePointer = fopen(fileName,"rt");
+ 
+        if (filePointer != NULL) 
+        {
+            fseek(filePointer, 0, SEEK_END);
+            count = ftell(filePointer);
+            rewind(filePointer);
+
+            if (count > 0) 
+            {
+                content = (char *)malloc(sizeof(char) * (count+1));
+                count = fread(content,sizeof(char),count,filePointer);
+                content[count] = '\0';
+            }
+            
+            fclose(filePointer);
+        }
+    }
+    return content;
+}
